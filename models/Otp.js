1621d686efe4e61265d851e763691ce176ba1e8c
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-16 20:44:02 +0530

    Mail functionality added

--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const sendMail = require("../utils/mailsender");
 
 const otpSchema = mongoose.Schema({
     email: {
@@ -15,6 +16,21 @@ const otpSchema = mongoose.Schema({
         default: Date.now(),
         expires: 5*60
     }
+})
+
+async function sendVerificationMail(email, otp) {
+    try{
+        const mailResponse = await sendMail(email, "Verification Email from EdTech", otp);
+        console.log("Email sent successfully!", mailResponse);
+    }catch(err){
+        console.log("Error while sending verification mail", err);
+        throw err;
+    }
+}
+
+otpSchema.pre("save", async function(next) {
+    await sendVerificationMail(this.email, this.otp);
+    next();
 })
 
 module.exports = mongoose.model("OTP", otpSchema);
