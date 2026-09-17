2763bb3ce2bbee0cdaf9a496880460017e8bb5f8
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-17 19:14:26 +0530

    signup functionality added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -77,6 +77,75 @@ async function sendOTP(req, res) {
 
 //signup function: 
 
+async function signup(req, res) {
+    try {
+        const {firstName, lastName, email, phone, createPassword, confirmPassword} = req.body;
+
+        let emailExists = await User.findOne(email);
+        if(emailExists) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists!"
+            })
+        }
+        
+        if(createPassword !== confirmPassword) {
+            return res.status(500).json({
+                success: false,
+                message: "Passwords do not match"
+            })
+        }
+
+        // const otp = sendOTP(req, res);
+        const otp = await OTP.find({email, otp});
+
+        await sendVerificationMail(email, otp);
+
+        const enteredOtp = req.body;
+
+
+        if(otp !== enteredOtp) {
+            return res.status(500).json({
+                success: false,
+                message: "OTP does not match!"
+            })
+        }
+
+        let hashedPassword;
+        try{
+            hashedPassword = bcrypt.hash(confirmPassword, 10);
+        }
+        catch(err){
+            return res.status(5090).json({
+                success: false,
+                message: "Error in hashing the password!"
+            })
+        }
+
+        const userData = {
+            firstName,
+            lastName,
+            email,
+            phone,
+            password: hashedPassword
+        }
+
+        const newUser = await User.create(userData)
+
+        return res.status(200).json({
+            success: true,
+            message: "User registered successfully!"
+        })
+
+    } catch (error) {
+        console.log("Error while registering the user: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Failure in registering the user"
+        })
+    }
+}
+
 
 
 module.exports = {sendOTP, signup}
